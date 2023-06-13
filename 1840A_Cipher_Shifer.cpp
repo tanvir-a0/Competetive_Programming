3f80791625f9bcd2ccb5bbc7ca347852205fa851
tanvir-a0 <t.a.porva@gmail.com>
2023-06-13 11:06:37 +0600

    Uploading a code

--- 1840A_Cipher_Shifer.cpp
+++ 1840A_Cipher_Shifer.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 int main()
 {
-    cout << "Testing 101" << endl;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        char s[n];
+        cin >> s;
+        char initial = s[0];
+        
+        for( int i = 1 ; i < n ; i = i + 1 )
+        {
+            //cout << t << "  " << s << "  " << s[n] << "  " << initial;
+
+            if( initial == s[i])
+            {
+                cout << initial;
+                if( i >= n )
+                {
+                    break;
+                }
+                initial = s[i+1];
+                i++;
+            }
+        }
+        cout << endl;
+    }
 
 }
